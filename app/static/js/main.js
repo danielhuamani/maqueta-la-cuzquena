da178b8c93d87aa3748c82d1e9ed39cdc6d2d3fb
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2016-09-10 16:59:16 -0500

    contacto

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,21 +1,52 @@
 jQuery(document).ready(function($) {
-	$(".vehiculos").click(function(event) {
-		/* Act on the event */
-		var valor_data = $(this).attr('data-vehiculo');
-		var class_active = "."+valor_data
-		/*$(".pop-active").removeClass('pop-active');*/
-		$(class_active).addClass('pop-active');
-		$(".overlay").addClass('overlay-block')
-	});
-	$(".icon-cross").click(function(event) {
-		/* Act on the event */
-		$(".pop-active").removeClass('pop-active');
-		$(".overlay").removeClass('overlay-block')
+	console.log($(".vehiculos"))
+	if($(".vehiculos").length > 0){
 
-	});
-	$(".overlay").click(function(event) {
+		$(".vehiculos").click(function(event) {
+			/* Act on the event */
+			var valor_data = $(this).attr('data-vehiculo');
+			var class_active = "."+valor_data
+			/*$(".pop-active").removeClass('pop-active');*/
+			$(class_active).addClass('pop-active');
+			$(".vehiculos-pages .overlay").addClass('overlay-block')
+		});
+		$(".icon-cross").click(function(event) {
+			/* Act on the event */
+			$(".pop-active").removeClass('pop-active');
+			console.log($("vehiculos-pages .overlay"))
+			$(".vehiculos-pages .overlay").removeClass('overlay-block')
+
+		});
+		$(".vehiculos-pages .overlay").click(function(event) {
+			/* Act on the event */
+			console.log($(this))
+			$(".pop-active").removeClass('pop-active');
+			$(".vehiculos-pages .overlay").removeClass('overlay-block')
+		});
+	}
+	$("header .overlay").click(function(event) {
+
+		$(".icon-menu").removeClass('res-menu-des')
+		$("header .close").removeClass('res-active')
+	    $("header .overlay").removeClass('res-active')
+	    $(".nav-principal").removeClass('nav-principal-active')
 		/* Act on the event */
-		$(".pop-active").removeClass('pop-active');
-		$(".overlay").removeClass('overlay-block')
+	});
+
+	$(".icon-menu").click(function(event) {
+
+	   $("header .close").addClass('res-active')
+	   $("header .overlay").addClass('res-active')
+	   $(".nav-principal").addClass('nav-principal-active')
+	   $("header .close").removeClass('res-menu-des')
+	   $(this).addClass('res-menu-des')
+	});
+	$("header .close").click(function(event) {
+		$(this).addClass('res-menu-des')
+		$(".icon-menu").removeClass('res-menu-des')
+		$("header .close").removeClass('res-active')
+	    $("header .overlay").removeClass('res-active')
+	    $(".nav-principal").removeClass('nav-principal-active')
+		/* Act on the event */
 	});
 });
